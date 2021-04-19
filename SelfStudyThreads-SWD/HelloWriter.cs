1ca69e793715be53275e6430b23600265e7e02cb
Mahmoud-SK <mahmoudkarzoun00@gmail.com>
2021-04-19 11:21:31 +0200

    Add project files.

---
+++ SelfStudyThreads-SWD/HelloWriter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SelfStudyThreads_SWD
+{
+	public class HelloWriter
+	{
+		string _name;
+		//int _numberOfTimesToLoop; Den ene måde
+
+		//public int _numberOfTimesToLeep { get; set; }
+
+		public int AmountOfTimeToSleep { get; set; }
+		public HelloWriter(string name)
+		{
+			_name = name;
+		}
+
+		public void SayHello(object numberoftimestoloop)
+		{
+			for (int i = 0; i < (int)numberoftimestoloop; i++)
+			{
+				Console.WriteLine($"Hello from {_name} #{i}");
+				Thread.Sleep(AmountOfTimeToSleep);
+			}
+		}
+	}
+}
---
+++ SelfStudyThreads-SWD/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace SelfStudyThreads_SWD
+{
+	class Programx
+	{
+		static void Main(string[] args)
+		{
+
+			bool yeet = true;
+			HelloWriter s1 = new HelloWriter("writer1");
+
+			HelloWriter s2 = new HelloWriter("writer2");
+
+			s1.AmountOfTimeToSleep = 200;
+			s2.AmountOfTimeToSleep = 500;
+
+			Thread t1 = new Thread(s1.SayHello);
+
+			Thread t2 = new Thread(s2.SayHello);
+
+			Thread t3 = new Thread(() => 
+			{
+				while (yeet)
+				{
+					Console.WriteLine("Never ending story");
+					Thread.Sleep(5000);
+				}
+			});
+
+
+			t1.Start(20);
+			t2.Start(20);
+			t3.Start();
+			t1.Join();
+			t2.Join();
+			yeet = false;
+			Console.WriteLine("Hello From Main");
+
+		}
+	}
+}
